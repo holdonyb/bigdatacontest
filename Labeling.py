5b81c553c6b3258577862682f81628188e17ca5c
holdonyb <holdonyb@gmail.com>
2015-01-25 22:06:33 +0800

    Update Labeling.py

--- Labeling.py
+++ Labeling.py
@@ -9,18 +9,18 @@ __author__ = 'holdo_000'
 import urllib
 import urllib2
 import codecs
-import datetime
-import cookielib
-import string
-import re
+#import datetime
+#import cookielib
+# string
+#import re
 import time
-import json
+#import json
 
 # set parameter
-old = 'CLOTHID'
+old = 'whatever'
 itemType = 'clothes'
-totalNumber = 20000
-startNumber = 100001
+totalNumber = 10
+startNumber = 100300
 #登录的主页面
 targetURL = r"http://image.baidu.com/i?objurl=http%3A%2F%2Fholdonyb.github.io%2Fbigdatacontest%2Fclothes_image%2F"+itemType+r"_"+old+r".jpg&rainbow=1&filename=&rt=0&rn=10&ftn=searchpcstu&ct=1&stt=0&tn=shituresultpc"
 
@@ -28,7 +28,7 @@ headers ={"User-agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (
           "Host":"image.baidu.com",
           "Referer":"image.baidu.com"}
 keyword = []
-f=codecs.open('keyword'+str(totalNumber)+'.txt','w','utf-8');
+f=codecs.open('keyword_'+itemType+'_'+str(startNumber)+'_'+str(startNumber+totalNumber)+'.txt','w','utf-8');
 start = time.clock()
 for i in range(0,totalNumber):
     print i
@@ -43,40 +43,3 @@ for i in range(0,totalNumber):
 f.close()
 end = time.clock()
 print end - start
-# ans = json.loads(responsedata.decode('utf8'))
-# captchaID = ans['retval']
-#
-# #####################################################
-# #获取验证码，并将其存为一个图片
-# requrl2= "http://www.gewara.com/captcha.xhtml?captchaId="+captchaID+ "&r=%d"  %rtime()
-# print requrl2
-# reqGetCaptcha= urllib2.Request(requrl2,headers=headers)
-# response2 = urllib2.urlopen(reqGetCaptcha)
-# imgdata = response2.read()
-# captchaName ='v.png'
-# imgfile = open(captchaName, "wb")
-# imgfile.write(imgdata)
-# imgfile.close()
-# t = ThreadClass()
-# t.start()
-# captcha=raw_input('Captcha:')
-# ############################################################
-# #构造Post数据，是从抓的包里分析得出的。
-# postData = {'TARGETURL' : '',
-#             'RememberMe' : 'Y',  #特有数据，不同网站可能不同
-#             'j_username' : username,#你的用户名
-#             'j_password' : password, #你的密码，密码可能是明文传输也可能是密文，如果是密文需要调用相应的加密算法加密
-#             'captchaId' : captchaID,   #特有数据，不同网站可能不同
-#             'captcha':captcha
-#             }
-# postData = dict(postData)
-# #需要给Post数据编码
-# postData = urllib.urlencode(postData)
-#
-# ############################################
-# #通过urllib2提供的request方法来向指定Url发送我们构造的数据，并完成登录过程
-# request = urllib2.Request(postURL, postData, headers)
-# # print request
-# response = opener.open(request)
-# trueurl = response.geturl()
-# text = response.read()
